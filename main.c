0dd60f620f2295629b7e5c1eccbcb5e95bccbeb1
Reagan Milchling <milchlingreagan@gmail.com>
2024-12-07 11:37:53 -0500

    refactor header

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -10,13 +9,13 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/wait.h>
-#include <signal.h>
 
 #define PORT "8080"
 #define BACKLOG 10
 #define MAXSIZE 2048
 #define PATHMAX 512
 #define MAXFILESIZE 4096
+#define HEADER_SIZE 16384
 
 void *get_in_addr(struct sockaddr *sa)
 {
@@ -38,33 +37,32 @@ long read_file(char** buffer, const char* file_path)
     long len = 0L;
     FILE* file = fopen(file_path, "rb");
 
-    if (file != NULL)
-    {
-        if (fseek(file, 0L, SEEK_END) == 0)
-        {
+    if (file != NULL) {
+        if (fseek(file, 0L, SEEK_END) == 0) {
             len = ftell(file);
-            if (fseek(file, 0L, SEEK_SET) != 0)
-            {
-                printf("failed to seek to start\n");
-            }
 
-            *buffer = calloc(len, sizeof(char));
+            if (fseek(file, 0L, SEEK_SET) != 0) {
+                perror("File seek set error:");
+            } else {
+                *buffer = calloc(len, sizeof(char));
+                int s = fread(*buffer, sizeof(char), len, file);
 
-            int s = fread(*buffer, sizeof(char), len, file);
-            if (ferror(file))
-            {
-                perror("file failed to load");
+                if (!ferror(file)) {
+                    fclose(file);
+                    return len;
+                }
             }
+        } else {
+            perror("File seek end error: ");
         }
-        else {
-            printf("failed to seek\n");
-        }
-        fclose(file);
-        printf("file size:%ld\n", len);
-        return len;
     }
-    printf("File was null\n");
-    return len;
+
+    perror("File open error: ");
+    if (file != NULL) free(*buffer);
+    file = NULL;
+
+    fclose(file);
+    return 0L;
 }
 
 int main(void)
@@ -84,6 +82,8 @@ int main(void)
     char connecting_addr[INET6_ADDRSTRLEN];
     char ip4[INET_ADDRSTRLEN] = "127.0.0.1";
 
+    char* header = calloc(HEADER_SIZE, sizeof(char));
+
     //-----------BEGIN SERVER STARTUP-----------
     int addr_status;
     struct addrinfo hints, *server_info;
@@ -99,7 +99,7 @@ int main(void)
 
     if ((addr_status = getaddrinfo(NULL, PORT, &hints, &server_info)) != 0)
     {
-        perror("getaddrinfo failure: ");
+        perror("Address info error: ");
         return(EXIT_FAILURE);
     }
     if ((server_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol)) < 0) 
@@ -157,27 +157,22 @@ whileLoop: while(1) {
         }
         path[path_index] = '\0';
 
-        printf("\nRequest:%s:%s:%s\n", ptcl, path, connecting_addr);
+        printf("\nRequest\t:%s:%s:%s\n", ptcl, path, connecting_addr);
 
-        if (is_home_dir(path))
-        {
-            printf("defaulting to index.html\n");
-            memcpy(path, "index.html\0", 11);
-        }
+        if (is_home_dir(path)) memcpy(path, "index.html\0", 11);
 
-        char* file_data;
-        long file_len = read_file(&file_data, path);
+        char* content;
+        long file_len = read_file(&content, path);
 
-        if (file_data == NULL)
+        if (file_len == 0L || content == NULL)
         {
             char *fail = "HTTP/1.1 404 Not found\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n\
-<HTML><HEAD><TITLE>File not found</TITLE></HEAD><BODY><H2>File not found</H2></BODY></HTML>";
-            printf("Serving 404\n");
+<HTML><HEAD><TITLE>404</TITLE></HEAD><BODY><H2>File not found</H2></BODY></HTML>";
+            printf("Serving: 404\n");
             send(new_socket, fail, strlen(fail), 0);
         }
         else
         {
-            int index = 0;
             char * mime;
             const char* extension = strchr(path, '.');
             if (strcmp(extension, ".css") == 0) {
@@ -200,26 +195,30 @@ whileLoop: while(1) {
                 mime = "text/plain";
             }
 
-            char *tmp = "HTTP/1.1 200 OK\r\nContent-Type: "; 
-            char *delim = "\r\n\r\n";
-            int start = strlen(tmp);
-            int mlen = strlen(mime);
-            long header_len = start + mlen + strlen(delim);
+            snprintf(header, HEADER_SIZE, 
+                     "HTTP/1.1 200 OK\r\n"
+                     "Content-Type: %s\r\n"
+                     "Content-Length: %li\r\n"
+                     "\r\n",
+                     mime, file_len);
+            int header_len = strlen(header);
+
             long resp_len = header_len + file_len;
-            char* response = malloc(resp_len);
-            memcpy(response, tmp, start);
-            memcpy(response + start, mime, mlen);
-            memcpy(response + start + mlen, delim, strlen(delim));
-            memcpy(response + header_len, file_data, file_len);
-            printf("Serving:header-%li:data-%li:total-%li:Mime-%s\n", header_len, file_len, resp_len, mime);
+            char* response = calloc(resp_len, sizeof(char));
+
+            memcpy(response, header, strlen(header));
+            memcpy(response + header_len, content, file_len);
+
+            printf("Serving:header-%i:data-%li:total-%li:Mime-%s\n", header_len, file_len, resp_len, mime);
 
             send(new_socket, response, resp_len, 0);
 
-            if (file_data != NULL) free(file_data);
+            if (content != NULL) free(content);
             if (response != NULL) free(response);
         }
 
         close(new_socket);
+        memset(header, 0, HEADER_SIZE);
         memset(buffer, 0, MAXSIZE);
         memset(ptcl, 0, 10);
         memset(path, 0, PATHMAX);
